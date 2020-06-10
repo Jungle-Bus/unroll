01cb84bc0a20a56b48fdca2f255828e5b363316e
Noémie <noemie.lehuby@zaclys.net>
2020-06-10 20:07:56 +0200

    add common js files

---
+++ commons.js
@@ -0,0 +1,6 @@
+function get_parameter_from_url(param_name) {
+    param_name = param_name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + param_name + "=([^&#]*)"),
+        results = regex.exec(location.href);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
--- route.js
+++ route.js
@@ -480,10 +480,3 @@ function display_error(error_message){
     `
     return template
 }
-
-function get_parameter_from_url(param_name) {
-    param_name = param_name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + param_name + "=([^&#]*)"),
-        results = regex.exec(location.href);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
